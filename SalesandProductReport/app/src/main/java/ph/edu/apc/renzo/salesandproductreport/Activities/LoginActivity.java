b42a42dca7326fb86247d286cd69bace064d4164
Jose Lorenzo Tadeo <renzotadeo26@gmail.com>
2016-10-27 21:02:49 +0800

    Sales and Product Report App

---
+++ SalesandProductReport/app/src/main/java/ph/edu/apc/renzo/salesandproductreport/Activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package ph.edu.apc.renzo.salesandproductreport.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ph.edu.apc.renzo.salesandproductreport.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
