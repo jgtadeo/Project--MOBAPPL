b548bc3c5d03b2583ce534bdf70a7a0cdf92b1c7
Jose Lorenzo Tadeo <renzotadeo26@gmail.com>
2016-11-16 15:28:22 +0800

    updated

--- SalesandProductReport/app/src/main/java/ph/edu/apc/renzo/salesandproductreport/Fragments/ExpensesFragment.java
+++ SalesandProductReport/app/src/main/java/ph/edu/apc/renzo/salesandproductreport/Fragments/ExpensesFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import ph.edu.apc.renzo.salesandproductreport.R;
 
@@ -14,6 +15,8 @@ import ph.edu.apc.renzo.salesandproductreport.R;
  */
 
 public class ExpensesFragment extends Fragment {
+
+    private Button add;
 
     public ExpensesFragment() {
 
@@ -29,6 +32,8 @@ public class ExpensesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_expenses, container, false);
+
+        add = (Button)view.findViewById(R.id.button_ExpensesAdd);
 
         return view;
     }
--- SalesandProductReport/app/src/main/java/ph/edu/apc/renzo/salesandproductreport/Fragments/SalesFragment.java
+++ SalesandProductReport/app/src/main/java/ph/edu/apc/renzo/salesandproductreport/Fragments/SalesFragment.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import ph.edu.apc.renzo.salesandproductreport.Model.Information;
 import ph.edu.apc.renzo.salesandproductreport.R;
 
 /**
@@ -20,8 +21,7 @@ public class SalesFragment extends Fragment {
     private EditText date, gross, bread, grocery, eload, smart, globe, sun;
     private Button compute;
     private String dateStr;
-    private float grossFlt, breadFlt, groceryFlt;
-    private int eloadFlt, smartFlt, globeFlt, sunFlt;
+    private int grossInt = 0, breadInt = 0, groceryInt = 0, eloadInt = 0, smartInt = 0, globeInt = 0, sunInt = 0;
 
     public SalesFragment() {
 
@@ -35,7 +35,7 @@ public class SalesFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sales, container, false);
 
@@ -52,15 +52,51 @@ public class SalesFragment extends Fragment {
         compute.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dateStr = date.getText().toString();
-                grossFlt = Float.parseFloat(gross.getText().toString());
-                breadFlt = Float.parseFloat(bread.getText().toString());
-                groceryFlt = Float.parseFloat(grocery.getText().toString());
-                eloadFlt = Integer.parseInt(eload.getText().toString());
-                smartFlt = Integer.parseInt(smart.getText().toString());
-                globeFlt = Integer.parseInt(globe.getText().toString());
-                sunFlt = Integer.parseInt(sun.getText().toString());
 
+                if (date.getText().toString().length() == 0) {
+                    date.setError("Date is required");
+                    return;
+                } else if (gross.getText().toString().length() == 0) {
+                    gross.setError("Gross Sale is required");
+                    return;
+                } else if (bread.getText().toString().length() == 0) {
+                    bread.setError("Bread is required");
+                    return;
+                } else if (grocery.getText().toString().length() == 0) {
+                    grocery.setError("Grocery is required");
+                    return;
+                } else if (eload.getText().toString().length() == 0) {
+                    eload.setError("Eload is required");
+                    return;
+                } else if (smart.getText().toString().length() == 0) {
+                    smart.setError("Smart is required");
+                    return;
+                } else if (globe.getText().toString().length() == 0) {
+                    globe.setError("Globe is required");
+                    return;
+                } else if (sun.getText().toString().length() == 0) {
+                    sun.setError("Sun is required");
+                    return;
+                } else {
+                    dateStr = date.getText().toString();
+                    grossInt = Integer.parseInt(gross.getText().toString());
+                    breadInt = Integer.parseInt(bread.getText().toString());
+                    groceryInt = Integer.parseInt(grocery.getText().toString());
+                    eloadInt = Integer.parseInt(eload.getText().toString());
+                    smartInt = Integer.parseInt(smart.getText().toString());
+                    globeInt = Integer.parseInt(globe.getText().toString());
+                    sunInt = Integer.parseInt(sun.getText().toString());
+
+                    Information information = new Information();
+                    information.setDate(dateStr);
+                    information.setGross(grossInt);
+                    information.setBread(breadInt);
+                    information.setGrocery(groceryInt);
+                    information.setEload(eloadInt);
+                    information.setSmart(smartInt);
+                    information.setGlobe(globeInt);
+                    information.setSun(sunInt);
+                }
             }
         });
         return view;
