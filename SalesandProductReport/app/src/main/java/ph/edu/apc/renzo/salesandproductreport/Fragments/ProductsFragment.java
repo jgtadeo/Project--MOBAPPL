3209ed1499800b7f5d68a34639692afe5bdf623b
jgtadeo <renzotadeo26@gmail.com>
2016-11-30 16:10:53 +0800

    updated application

--- SalesandProductReport/app/src/main/java/ph/edu/apc/renzo/salesandproductreport/Fragments/ProductsFragment.java
+++ SalesandProductReport/app/src/main/java/ph/edu/apc/renzo/salesandproductreport/Fragments/ProductsFragment.java
@@ -1,20 +1,31 @@
 package ph.edu.apc.renzo.salesandproductreport.Fragments;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Paint;
+import android.preference.DialogPreference;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.TextViewCompat;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
+import ph.edu.apc.renzo.salesandproductreport.Activities.LoginActivity;
 import ph.edu.apc.renzo.salesandproductreport.R;
 
 /**
  * Created by Renzo on 12/11/2016.
  */
 
-public class ProductsFragment extends Fragment {
+public class ProductsFragment extends Fragment implements View.OnClickListener {
+
+    private TextView logout;
 
     public ProductsFragment() {
 
@@ -31,6 +42,37 @@ public class ProductsFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_products, container, false);
 
+        logout = (TextView)view.findViewById(R.id.textView_ProductsLogout);
+        logout.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+        logout.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.textView_ProductsLogout:
+                AlertDialog DYWLogout = new AlertDialog.Builder(getActivity()).create();
+                DYWLogout.setTitle("Logout");
+                DYWLogout.setMessage("Do you really want to logout?");
+                DYWLogout.setButton(AlertDialog.BUTTON_POSITIVE, "Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        Intent logout = new Intent(getActivity(), LoginActivity.class);
+                        startActivity(logout);
+                        getActivity().finish();
+                    }
+                });
+                DYWLogout.setButton(AlertDialog.BUTTON_NEGATIVE, "No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                DYWLogout.show();
+                break;
+        }
     }
 }
--- SalesandProductReport/app/src/main/java/ph/edu/apc/renzo/salesandproductreport/Fragments/SalesFragment.java
+++ SalesandProductReport/app/src/main/java/ph/edu/apc/renzo/salesandproductreport/Fragments/SalesFragment.java
@@ -1,15 +1,21 @@
 package ph.edu.apc.renzo.salesandproductreport.Fragments;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Paint;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import ph.edu.apc.renzo.salesandproductreport.Activities.LoginActivity;
 import ph.edu.apc.renzo.salesandproductreport.Model.Information;
 import ph.edu.apc.renzo.salesandproductreport.Model.SalesProductDBAdapter;
 import ph.edu.apc.renzo.salesandproductreport.R;
@@ -18,10 +24,11 @@ import ph.edu.apc.renzo.salesandproductreport.R;
  * Created by Renzo on 01/11/2016.
  */
 
-public class SalesFragment extends Fragment {
+public class SalesFragment extends Fragment implements View.OnClickListener{
 
     private SalesProductDBAdapter salesproductDBAdapter = new SalesProductDBAdapter(getContext());
     private EditText date, gross, bread, grocery, eload, smart, globe, sun;
+    private TextView logout;
     private Button compute;
     private String dateStr;
     private int grossInt = 0, breadInt = 0, groceryInt = 0, eloadInt = 0, smartInt = 0, globeInt = 0, sunInt = 0;
@@ -50,6 +57,10 @@ public class SalesFragment extends Fragment {
         smart = (EditText)view.findViewById(R.id.editText_SaleSmart);
         globe = (EditText)view.findViewById(R.id.editText_SaleGlobe);
         sun = (EditText)view.findViewById(R.id.editText_SaleSun);
+
+        logout = (TextView)view.findViewById(R.id.textView_SalesLogout);
+        logout.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+        logout.setOnClickListener(this);
 
         compute = (Button)view.findViewById(R.id.button_SalesCompute);
         compute.setOnClickListener(new View.OnClickListener() {
@@ -110,6 +121,33 @@ public class SalesFragment extends Fragment {
         return view;
     }
 
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.textView_SalesLogout:
+                AlertDialog DYWlogout = new AlertDialog.Builder(getActivity()).create();
+                DYWlogout.setTitle("Logout");
+                DYWlogout.setMessage("Do you really want to logout?");
+                DYWlogout.setButton(AlertDialog.BUTTON_POSITIVE, "Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        Intent logout = new Intent(getActivity(), LoginActivity.class);
+                        startActivity(logout);
+                        getActivity().finish();
+                    }
+                });
+                DYWlogout.setButton(AlertDialog.BUTTON_NEGATIVE, "No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                DYWlogout.show();
+                break;
+        }
+    }
 }
 
 
