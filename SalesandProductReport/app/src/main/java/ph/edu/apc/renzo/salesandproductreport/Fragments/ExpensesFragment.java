0fc28695b18c53a414db49528cefd32d19d976e3
Christian Carlos <shinzhouu@gmail.com>
2016-12-01 20:40:47 +0800

    expenses frag update

--- SalesandProductReport/app/src/main/java/ph/edu/apc/renzo/salesandproductreport/Fragments/ExpensesFragment.java
+++ SalesandProductReport/app/src/main/java/ph/edu/apc/renzo/salesandproductreport/Fragments/ExpensesFragment.java
@@ -1,5 +1,6 @@
 package ph.edu.apc.renzo.salesandproductreport.Fragments;
 
+import android.graphics.Paint;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import ph.edu.apc.renzo.salesandproductreport.R;
 
@@ -14,9 +17,14 @@ import ph.edu.apc.renzo.salesandproductreport.R;
  * Created by Renzo on 12/11/2016.
  */
 
-public class ExpensesFragment extends Fragment {
+public class ExpensesFragment extends Fragment implements View.OnClickListener{
 
-    private Button add;
+    private Button addmore;
+    private Button done;
+    private EditText date, expense, totalcost;
+    private TextView logout;
+    private String dateEx, nameEx;
+    private int totalcostEx = 0;
 
     public ExpensesFragment() {
 
@@ -33,8 +41,20 @@ public class ExpensesFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_expenses, container, false);
 
-        add = (Button)view.findViewById(R.id.button_ExpensesAdd);
+        date = (EditText) view.findViewById(R.id.editText_date);
+        expense = (EditText) view.findViewById(R.id.editText_expense);
+        totalcost = (EditText) view.findViewById(R.id.editText_totalcost);
+
+        logout = (TextView)view.findViewById(R.id.textView_ExpensesLogout);
+        logout.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+        logout.setOnClickListener(this);
+
 
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
